61fa7ec636dd79457f5538ed1b9651a4de5e01e3
shen111x <sycombike@gmail.com>
2026-07-16 22:21:52 -0700

    asd
    asd

--- docs/components/delajii-terminal/asset/rooms/lounge/create-lounge.js
+++ docs/components/delajii-terminal/asset/rooms/lounge/create-lounge.js
@@ -2,7 +2,7 @@ import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.185.1/examples/
 
 // Public size handle for this asset. Change height, or pass an override to
 // createLounge(), to resize the model uniformly in world units.
-export const LOUNGE_SIZE_HANDLE = Object.freeze({ height: 8 });
+export const LOUNGE_SIZE_HANDLE = Object.freeze({ height: 10 });
 export const LOUNGE_FLOOR_OFFSET = 0.01;
 
 function createWallBoundary(THREE, object, targetHeight) {
